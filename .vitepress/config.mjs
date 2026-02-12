67d63c16c11c54bbc5c996288d3df53ec1eb2fa8
jiwon <dlwldnjs9690@gmail.com>
2026-02-12 18:35:53 +0900

    set sitemap, robot

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -1,9 +1,25 @@
 import { defineConfig } from 'vitepress'
-
 // https://vitepress.dev/reference/site-config
+
+
 export default defineConfig({
   title: "My SWIM BLOG Project",
-  description: "이지스윔 블로그",
+  description: "수영장 자유수영 후기, 수영대회 리뷰, 여행일지를 기록하는 블로그입니다.",
+
+  head: [
+    ['meta', { name: 'author', content: 'Jiwon Lee' }],
+    ['meta', { name: 'keywords', content: '수영장, 자유수영 후기, 수영대회 리뷰, swimming pool, Korea masters swimming' }],
+
+    // Open Graph (카톡, 페북, SNS 공유용)
+    ['meta', { property: 'og:type', content: 'website' }],
+    ['meta', { property: 'og:title', content: 'Jiwon Swim & Dev Blog' }],
+    ['meta', { property: 'og:description', content: 'Swimming reviews, travel diaries, and development notes.' }],
+    ['meta', { property: 'og:url', content: 'https://ljw322.github.io/' }],
+
+    // 트위터 카드
+    ['meta', { name: 'twitter:card', content: 'summary' }]
+  ],
+
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
@@ -25,7 +41,12 @@ export default defineConfig({
     ],
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+      { icon: 'github', link: 'https://github.com/ljw322' }
     ]
-  }
+  },
+
+  sitemap: {
+    hostname: 'https://ljw322.github.io',
+  },
+
 })
